e2330bf54420cc9acae1ea37bd32c3908c40deac
Jan Eckert <jan.eckert@tum.de>
2021-10-22 10:46:13 +0200

    initial implementation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # dot
 
 scanner library for parser development
+
+Based on [this](https://depth-first.com/articles/2019/01/22/scanner-driven-parser-development/) article by (Richard L. Apodaca)[https://depth-first.com/about/].
---
+++ src/lib/scanner.ts
@@ -0,0 +1,42 @@
+/*
+Implemented from https://depth-first.com/articles/2019/01/22/scanner-driven-parser-development/
+To be extended
+*/
+
+export class Scanner {
+
+  input: string;
+  private _cursor: number;
+
+  constructor(input: string) {
+    this.input = input;
+    this._cursor = 0;
+  }
+
+  get cursor(): number {
+    return this.cursor;
+  }
+
+  peek(): string {
+    return this.input[this._cursor]
+  }
+
+  pop(): string {
+    return this.input[this._cursor++]
+  }
+
+  scan(regex: RegExp): string | undefined {
+    if(!regex.global) {
+      throw Error("For a Scanner-Regex, the global flag must be set")
+      return undefined
+    }
+
+    regex.lastIndex = this._cursor
+    const match = regex.exec(this.input)
+
+    if(match === null || match.index !== this._cursor) return undefined
+
+    this._cursor = regex.lastIndex
+    return match[0]
+  }
+}
